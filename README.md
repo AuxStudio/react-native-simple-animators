6a84dc17a272274817fae4c5a05d2d4cfb4c87f5
Shaun Saker <shaun@aux.co.za>
2018-05-11 17:35:04 +0200

    Updated formatting

--- README.md
+++ README.md
@@ -7,18 +7,18 @@ Currently supported styles: height, opacity, rotate, scale, translateX, translat
 ## Installation
 
 ```shell
-npm install --save react-native-simple-animators
+yard add react-native-simple-animators
 ```
 
 ## Importing
 
 ```js
-import { AnimateTranslateX } from 'react-native-simple-animators'; // ES6
+import { AnimateTranslateX } from 'react-native-simple-animators';
 ```
 
 ## Usage
 
-These animators are intended to animate their children in what I feel is a very simple way. 
+These animators are intended to animate their children in what I feel is a very simple way.
 Opacity and transform styles will use the nativeDriver which greatly improves performance.
 Here is an example of using AnimateTranslateX to animate and wrap a simple Text component:
 
@@ -28,26 +28,27 @@ import { Text, Easing } from 'react-native';
 import { AnimateTranslateX } from 'react-native-simple-animators';
 
 class BannerText extends React.Component {
-    render() {
-        return (
-            <AnimateTranslateX
-                initialValue={-200} // value to animate from
-                finalValue={0} // value to animate to
-                shouldAnimateIn // flag to start the animation
-                animateInCallBack={null} // some function to trigger once the animation has ended
-                shouldAnimateOut={false} // flag to reverse the animation (only if shouldAnimateIn was previously set)
-                animateOutCallBack={null} // some function to trigger once the animation has ended
-                shouldRepeat // repeat the animation, ie. -200 => 0 and back to -200 etc.
-                shouldLoop // loop the animation (must set shouldRepeat to work), ie. -200 => 0 => reset to 0 => -200 => 0 etc.
-                duration={2000} // duration of the animation
-                easing={Easing.ease} // easing
-                delay={500} // useful with animating lists (DELAY * index)
-                style={{backgroundColor: 'red'}} // style(s) of the wrapper (array or object)
-                >
-                <Text>How easy was this?</Text>
-            </AnimateTranslateX>
-        )
-    }
+  render() {
+    return (
+      <AnimateTranslateX
+        initialValue={-200} // value to animate from
+        finalValue={0} // value to animate to
+        shouldAnimateIn // flag to start the animation
+        animateInCallBack={null} // some function to trigger once the animation has ended
+        shouldAnimateOut={false} // flag to reverse the animation (only if shouldAnimateIn was previously set)
+        animateOutCallBack={null} // some function to trigger once the animation has ended
+        shouldRepeat // repeat the animation, ie. -200 => 0 and back to -200 etc.
+        shouldLoop // loop the animation (must set shouldRepeat to work), ie. -200 => 0 => reset to 0 => -200 => 0 etc.
+        duration={2000} // duration of the animation
+        easing={Easing.ease} // easing
+        delay={500} // useful with animating lists (DELAY * index)
+        style={{ backgroundColor: 'red' }} // style(s) of the wrapper (array or object)
+      >
+        <Text>How easy was this?</Text>
+      </AnimateTranslateX>
+    );
+  }
 }
-
 ```
+
+## TODOS
--- src/AnimateOpacity.js
+++ src/AnimateOpacity.js
@@ -1,125 +1,122 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Animated, Easing } from "react-native";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Animated, Easing } from 'react-native';
 
 export default class AnimateOpacity extends React.Component {
-    constructor(props) {
-        super(props);
+  static get propTypes() {
+    return {
+      initialValue: PropTypes.number,
+      finalValue: PropTypes.number,
+      shouldAnimateIn: PropTypes.bool,
+      shouldAnimateOut: PropTypes.bool,
+      animateInCallBack: PropTypes.func,
+      animateOutCallBack: PropTypes.func,
+      shouldRepeat: PropTypes.bool,
+      shouldLoop: PropTypes.bool, // used with repeat (cycles the animation instead of going back and forth)
+      style: PropTypes.oneOfType([PropTypes.number, PropTypes.object, PropTypes.array]),
+      duration: PropTypes.number,
+      easing: PropTypes.func,
+      delay: PropTypes.number,
+      children: PropTypes.node,
+    };
+  }
 
-        this.animateIn = this.animateIn.bind(this);
-        this.animateOut = this.animateOut.bind(this);
+  static get defaultProps() {
+    return {
+      duration: 250,
+      easing: Easing.gentle,
+    };
+  }
 
-        this.state = {
-            animatedValue: new Animated.Value(0),
-        };
+  state = {
+    animatedValue: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    // In cases where update needs to be used instead
+    const shouldAnimateOnMount =
+      (this.props.initialValue || this.props.initialValue === 0) &&
+      (this.props.finalValue || this.props.finalValue === 0);
+
+    if (this.props.shouldAnimateIn && shouldAnimateOnMount) {
+      this.animateIn();
     }
+  }
 
-    static get propTypes() {
-        return {
-            initialValue: PropTypes.number,
-            finalValue: PropTypes.number,
-            shouldAnimateIn: PropTypes.bool,
-            shouldAnimateOut: PropTypes.bool,
-            animateInCallBack: PropTypes.func,
-            animateOutCallBack: PropTypes.func,
-            shouldRepeat: PropTypes.bool,
-            shouldLoop: PropTypes.bool, // used with repeat (cycles the animation instead of going back and forth)
-            style: PropTypes.oneOfType([
-                PropTypes.number,
-                PropTypes.object,
-                PropTypes.array,
-            ]),
-            duration: PropTypes.number,
-            easing: PropTypes.object,
-            delay: PropTypes.number,
-        };
+  componentDidUpdate(prevProps) {
+    // Use case where update used as onMount (see componentDidMount)
+    const shouldAnimateOnUpdate =
+      (this.props.initialValue && this.props.initialValue !== prevProps.initialValue) ||
+      (this.props.finalValue && this.props.finalValue !== prevProps.finalValue);
+
+    if (shouldAnimateOnUpdate) {
+      this.animateIn();
+    } else if (
+      this.props.shouldAnimateIn &&
+      this.props.shouldAnimateIn !== prevProps.shouldAnimateIn
+    ) {
+      this.animateIn();
+    } else if (
+      this.props.shouldAnimateOut &&
+      this.props.shouldAnimateOut !== prevProps.shouldAnimateOut
+    ) {
+      this.animateOut();
     }
+  }
 
-    static get defaultProps() {
-        return {
-            duration: 250,
-            easing: Easing.gentle,
-        };
-    }
+  animateIn = () => {
+    Animated.timing(this.state.animatedValue, {
+      toValue: 1,
+      duration: this.props.duration,
+      easing: this.props.easing,
+      useNativeDriver: true,
+      delay: this.props.delay,
+    }).start(() => {
+      if (this.props.animateInCallBack) {
+        this.props.animateInCallBack();
+      }
 
-    componentDidMount() {
-        // In cases where update needs to be used instead
-        const shouldAnimateOnMount =
-            (this.props.initialValue || this.props.initialValue === 0) &&
-            (this.props.finalValue || this.props.finalValue === 0);
-
-        this.props.shouldAnimateIn && shouldAnimateOnMount && this.animateIn();
-    }
-
-    componentDidUpdate(prevProps) {
-        // Use case where update used as onMount (see componentDidMount)
-        const shouldAnimateOnUpdate =
-            (this.props.initialValue &&
-                this.props.initialValue !== prevProps.initialValue) ||
-            (this.props.finalValue &&
-                this.props.finalValue !== prevProps.finalValue);
-
-        if (shouldAnimateOnUpdate) {
-            this.animateIn();
-        } else if (
-            this.props.shouldAnimateIn &&
-            this.props.shouldAnimateIn !== prevProps.shouldAnimateIn
-        ) {
-            this.animateIn();
-        } else if (
-            this.props.shouldAnimateOut &&
-            this.props.shouldAnimateOut !== prevProps.shouldAnimateOut
-        ) {
-            this.animateOut();
+      if (this.props.shouldRepeat) {
+        if (this.props.shouldLoop) {
+          this.state.animatedValue.setValue(0);
+          this.animateIn();
+        } else {
+          this.animateOut();
         }
-    }
+      }
+    });
+  };
 
-    animateIn() {
-        Animated.timing(this.state.animatedValue, {
-            toValue: 1,
-            duration: this.props.duration,
-            easing: this.props.easing,
-            useNativeDriver: true,
-            delay: this.props.delay,
-        }).start(() => {
-            this.props.animateInCallBack && this.props.animateInCallBack();
+  animateOut = () => {
+    Animated.timing(this.state.animatedValue, {
+      toValue: 0,
+      duration: this.props.duration,
+      easing: this.props.easing,
+      useNativeDriver: true,
+      delay: this.props.delay,
+    }).start(() => {
+      if (this.props.animateOutCallBack) {
+        this.props.animateOutCallBack();
+      }
 
-            if (this.props.shouldRepeat) {
-                if (this.props.shouldLoop) {
-                    this.state.animatedValue.setValue(0);
-                    this.animateIn();
-                } else {
-                    this.animateOut();
-                }
-            }
-        });
-    }
+      if (this.props.shouldRepeat) {
+        this.animateIn();
+      }
+    });
+  };
 
-    animateOut() {
-        Animated.timing(this.state.animatedValue, {
-            toValue: 0,
-            duration: this.props.duration,
-            easing: this.props.easing,
-            useNativeDriver: true,
-            delay: this.props.delay,
-        }).start(() => {
-            this.props.animateOutCallBack && this.props.animateOutCallBack();
-            this.props.shouldRepeat && this.animateIn();
-        });
-    }
+  render() {
+    const animatedStyles = {
+      opacity: this.state.animatedValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: [this.props.initialValue, this.props.finalValue],
+      }),
+    };
 
-    render() {
-        const animatedStyles = {
-            opacity: this.state.animatedValue.interpolate({
-                inputRange: [0, 1],
-                outputRange: [this.props.initialValue, this.props.finalValue],
-            }),
-        };
-
-        return (
-            <Animated.View style={[this.props.style, animatedStyles]}>
-                {this.props.children}
-            </Animated.View>
-        );
-    }
+    return (
+      <Animated.View style={[this.props.style, animatedStyles]}>
+        {this.props.children}
+      </Animated.View>
+    );
+  }
 }
