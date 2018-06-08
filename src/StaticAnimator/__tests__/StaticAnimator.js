32f20ae3b33963115a47f50617eda909cdd429ef
Shaun Saker <shaun@aux.co.za>
2018-06-08 18:01:49 +0200

    Added more tests for StaticAnimator

--- src/StaticAnimator/__tests__/StaticAnimator.js
+++ src/StaticAnimator/__tests__/StaticAnimator.js
@@ -4,9 +4,9 @@ import { View } from 'react-native';
 
 import StaticAnimator from '../';
 
-// FIXME: shouldRepeat and shouldLoop were occluded from these tests
+// FIXME: shouldAnimateOut, delay, duration, shouldRepeat and shouldLoop were occluded from these tests
 // due to the async complexity they introduce
-it('renders a StaticAnimator with a height animation at 0 animating in', () => {
+it('renders a StaticAnimator with a height animation to 100', () => {
   expect(
     renderer.create(
       <StaticAnimator
@@ -16,8 +16,91 @@ it('renders a StaticAnimator with a height animation at 0 animating in', () => {
         shouldAnimateIn
         animateInCallback={jest.fn()}
         easing={jest.fn()}
-        duration={500}
-        delay={500}
+      >
+        <View style={{ width: 100, height: 100 }} />
+      </StaticAnimator>,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a StaticAnimator with a height animation to 50', () => {
+  expect(
+    renderer.create(
+      <StaticAnimator
+        type="height"
+        initialValue={0}
+        finalValue={50}
+        shouldAnimateIn
+        animateInCallback={jest.fn()}
+        easing={jest.fn()}
+      >
+        <View style={{ width: 100, height: 100 }} />
+      </StaticAnimator>,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a StaticAnimator with a height animation to 0', () => {
+  expect(
+    renderer.create(
+      <StaticAnimator
+        type="height"
+        initialValue={0}
+        finalValue={0}
+        shouldAnimateIn
+        animateInCallback={jest.fn()}
+        easing={jest.fn()}
+      >
+        <View style={{ width: 100, height: 100 }} />
+      </StaticAnimator>,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a StaticAnimator with a height animation to 0', () => {
+  expect(
+    renderer.create(
+      <StaticAnimator
+        type="height"
+        initialValue={0}
+        finalValue={0}
+        shouldAnimateIn
+        animateInCallback={jest.fn()}
+        easing={jest.fn()}
+      >
+        <View style={{ width: 100, height: 100 }} />
+      </StaticAnimator>,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a StaticAnimator with a translateX animation to 100', () => {
+  expect(
+    renderer.create(
+      <StaticAnimator
+        type="translateX"
+        initialValue={0}
+        finalValue={100}
+        shouldAnimateIn
+        animateInCallback={jest.fn()}
+        easing={jest.fn()}
+      >
+        <View style={{ width: 100, height: 100 }} />
+      </StaticAnimator>,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a StaticAnimator with an opacityt animation to 1', () => {
+  expect(
+    renderer.create(
+      <StaticAnimator
+        type="height"
+        initialValue={0}
+        finalValue={1}
+        shouldAnimateIn
+        animateInCallback={jest.fn()}
+        easing={jest.fn()}
       >
         <View style={{ width: 100, height: 100 }} />
       </StaticAnimator>,
