7898e8137c8abc3e1dcc31c7733a354e316aca88
Shaun Saker <shaun@aux.co.za>
2018-06-08 17:41:15 +0200

    Add tests for the EventAnimator

---
+++ src/EventAnimator/__tests__/EventAnimator.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Animated } from 'react-native';
+
+import EventAnimator from '../';
+
+it('renders a EventAnimator with a height animation at 0', () => {
+  expect(
+    renderer.create(
+      <EventAnimator
+        type="height"
+        animatedValue={new Animated.Value(0)}
+        interpolation={{ inputRange: [0, 100], outputRange: [0, 100], extrapolate: 'clamp' }}
+      />,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a EventAnimator with a height animation at 50', () => {
+  expect(
+    renderer.create(
+      <EventAnimator
+        type="height"
+        animatedValue={new Animated.Value(50)}
+        interpolation={{ inputRange: [0, 100], outputRange: [0, 100], extrapolate: 'clamp' }}
+      />,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a EventAnimator with a height animation at 100', () => {
+  expect(
+    renderer.create(
+      <EventAnimator
+        type="height"
+        animatedValue={new Animated.Value(100)}
+        interpolation={{ inputRange: [0, 100], outputRange: [0, 100], extrapolate: 'clamp' }}
+      />,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a EventAnimator with an translateX animation', () => {
+  expect(
+    renderer.create(
+      <EventAnimator
+        type="translateX"
+        animatedValue={new Animated.Value(0)}
+        interpolation={{ inputRange: [0, 100], outputRange: [0, 100], extrapolate: 'clamp' }}
+      />,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a EventAnimator with an opacity animation', () => {
+  expect(
+    renderer.create(
+      <EventAnimator
+        type="opacity"
+        animatedValue={new Animated.Value(0)}
+        interpolation={{ inputRange: [0, 1], outputRange: [0, 100], extrapolate: 'clamp' }}
+      />,
+    ),
+  ).toMatchSnapshot();
+});
