7fa11bbb7b5a0b62b028a94b82be37f8aa602bf5
Shaun Saker <shaun@aux.co.za>
2018-06-21 05:09:18 +0200

    Update StaticAnimator tests

--- src/StaticAnimator/__tests__/StaticAnimator.test.js
+++ src/StaticAnimator/__tests__/StaticAnimator.test.js
@@ -6,104 +6,106 @@ import StaticAnimator from '../';
 
 // FIXME: shouldAnimateOut, delay, duration, shouldRepeat and shouldLoop were occluded from these tests
 // due to the async complexity they introduce
-it('renders a StaticAnimator with a height animation to 100', () => {
-  expect(
-    renderer.create(
-      <StaticAnimator
-        type="height"
-        initialValue={0}
-        finalValue={100}
-        shouldAnimateIn
-        animateInCallback={jest.fn()}
-        easing={jest.fn()}
-      >
-        <View style={{ width: 100, height: 100 }} />
-      </StaticAnimator>,
-    ),
-  ).toMatchSnapshot();
-});
+describe('StaticAnimator', () => {
+  it('renders with a height animation to 100', () => {
+    expect(
+      renderer.create(
+        <StaticAnimator
+          type="height"
+          initialValue={0}
+          finalValue={100}
+          shouldAnimateIn
+          animateInCallback={jest.fn()}
+          easing={jest.fn()}
+        >
+          <View style={{ width: 100, height: 100 }} />
+        </StaticAnimator>,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a StaticAnimator with a height animation to 50', () => {
-  expect(
-    renderer.create(
-      <StaticAnimator
-        type="height"
-        initialValue={0}
-        finalValue={50}
-        shouldAnimateIn
-        animateInCallback={jest.fn()}
-        easing={jest.fn()}
-      >
-        <View style={{ width: 100, height: 100 }} />
-      </StaticAnimator>,
-    ),
-  ).toMatchSnapshot();
-});
+  it('renders with a height animation to 50', () => {
+    expect(
+      renderer.create(
+        <StaticAnimator
+          type="height"
+          initialValue={0}
+          finalValue={50}
+          shouldAnimateIn
+          animateInCallback={jest.fn()}
+          easing={jest.fn()}
+        >
+          <View style={{ width: 100, height: 100 }} />
+        </StaticAnimator>,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a StaticAnimator with a height animation to 0', () => {
-  expect(
-    renderer.create(
-      <StaticAnimator
-        type="height"
-        initialValue={0}
-        finalValue={0}
-        shouldAnimateIn
-        animateInCallback={jest.fn()}
-        easing={jest.fn()}
-      >
-        <View style={{ width: 100, height: 100 }} />
-      </StaticAnimator>,
-    ),
-  ).toMatchSnapshot();
-});
+  it('renders with a height animation to 0', () => {
+    expect(
+      renderer.create(
+        <StaticAnimator
+          type="height"
+          initialValue={0}
+          finalValue={0}
+          shouldAnimateIn
+          animateInCallback={jest.fn()}
+          easing={jest.fn()}
+        >
+          <View style={{ width: 100, height: 100 }} />
+        </StaticAnimator>,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a StaticAnimator with a height animation to 0', () => {
-  expect(
-    renderer.create(
-      <StaticAnimator
-        type="height"
-        initialValue={0}
-        finalValue={0}
-        shouldAnimateIn
-        animateInCallback={jest.fn()}
-        easing={jest.fn()}
-      >
-        <View style={{ width: 100, height: 100 }} />
-      </StaticAnimator>,
-    ),
-  ).toMatchSnapshot();
-});
+  it('renders with a height animation to 0', () => {
+    expect(
+      renderer.create(
+        <StaticAnimator
+          type="height"
+          initialValue={0}
+          finalValue={0}
+          shouldAnimateIn
+          animateInCallback={jest.fn()}
+          easing={jest.fn()}
+        >
+          <View style={{ width: 100, height: 100 }} />
+        </StaticAnimator>,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a StaticAnimator with a translateX animation to 100', () => {
-  expect(
-    renderer.create(
-      <StaticAnimator
-        type="translateX"
-        initialValue={0}
-        finalValue={100}
-        shouldAnimateIn
-        animateInCallback={jest.fn()}
-        easing={jest.fn()}
-      >
-        <View style={{ width: 100, height: 100 }} />
-      </StaticAnimator>,
-    ),
-  ).toMatchSnapshot();
-});
+  it('renders with a translateX animation to 100', () => {
+    expect(
+      renderer.create(
+        <StaticAnimator
+          type="translateX"
+          initialValue={0}
+          finalValue={100}
+          shouldAnimateIn
+          animateInCallback={jest.fn()}
+          easing={jest.fn()}
+        >
+          <View style={{ width: 100, height: 100 }} />
+        </StaticAnimator>,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a StaticAnimator with an opacity animation to 1', () => {
-  expect(
-    renderer.create(
-      <StaticAnimator
-        type="height"
-        initialValue={0}
-        finalValue={1}
-        shouldAnimateIn
-        animateInCallback={jest.fn()}
-        easing={jest.fn()}
-      >
-        <View style={{ width: 100, height: 100 }} />
-      </StaticAnimator>,
-    ),
-  ).toMatchSnapshot();
+  it('renders with an opacity animation to 1', () => {
+    expect(
+      renderer.create(
+        <StaticAnimator
+          type="height"
+          initialValue={0}
+          finalValue={1}
+          shouldAnimateIn
+          animateInCallback={jest.fn()}
+          easing={jest.fn()}
+        >
+          <View style={{ width: 100, height: 100 }} />
+        </StaticAnimator>,
+      ),
+    ).toMatchSnapshot();
+  });
 });
