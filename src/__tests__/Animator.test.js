ca8a5b562eb33f8988870bdd8ece3a0079853f0c
Shaun Saker <shaun@aux.co.za>
2018-06-21 05:07:30 +0200

    Update Animator tests

--- src/__tests__/Animator.test.js
+++ src/__tests__/Animator.test.js
@@ -4,29 +4,31 @@ import { Animated } from 'react-native';
 
 import Animator from '../';
 
-it('renders a EventAnimator from Animator when animatedValue was passed in', () => {
-  expect(
-    renderer.create(
-      <Animator
-        type="height"
-        animatedValue={new Animated.Value(0)}
-        interpolation={{ inputRange: [0, 1], outputRange: [0, 100], extrapolate: 'clamp' }}
-      />,
-    ),
-  ).toMatchSnapshot();
-});
+describe('EventAnimator', () => {
+  it('renders an EventAnimator when animatedValue was passed in', () => {
+    expect(
+      renderer.create(
+        <Animator
+          type="height"
+          animatedValue={new Animated.Value(0)}
+          interpolation={{ inputRange: [0, 1], outputRange: [0, 100], extrapolate: 'clamp' }}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a StaticAnimator from Animator when animatedValue was not passed in', () => {
-  expect(
-    renderer.create(
-      <Animator
-        type="height"
-        initialValue={0}
-        finalValue={1}
-        shouldAnimateIn
-        animateInCallback={jest.fn()}
-        easing={jest.fn()}
-      />,
-    ),
-  ).toMatchSnapshot();
+  it('renders a StaticAnimator when animatedValue was not passed in', () => {
+    expect(
+      renderer.create(
+        <Animator
+          type="height"
+          initialValue={0}
+          finalValue={1}
+          shouldAnimateIn
+          animateInCallback={jest.fn()}
+          easing={jest.fn()}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
 });
